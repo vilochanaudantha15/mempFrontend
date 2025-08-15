b58f00006250411d66e71856ff894639deab517e
Vilochana_Udantha <138508724+vilochanaudantha15@users.noreply.github.com>
2025-08-15 12:38:36 +0530

    Update CurrentStock.jsx

--- src/components/currentStock/CurrentStock.jsx
+++ src/components/currentStock/CurrentStock.jsx
@@ -3,7 +3,7 @@ import { FaBox, FaCog, FaCubes, FaThumbtack, FaTv, FaSearch } from "react-icons/
 import { motion } from "framer-motion";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./CurrentStock.scss";
+import "./currentStock.scss";
 
 const API_BASE_URL = "/api"; // For office server with reverse proxy
 
